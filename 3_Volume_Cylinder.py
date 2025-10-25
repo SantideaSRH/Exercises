39a46132bc4e23b0f893816eae8439324d3916b7
Santiago De Alba Cazares <santiago.dealbacazares@stud.srh-university.de>
2025-10-25 12:46:43 +0200

    Revert "no message"
    This reverts commit cd032031a13d9a88344fccf479b7574ec0fa7746.

--- 3_Volume_Cylinder.py
+++
@@ -1,5 +0,0 @@
-hight = int(input("Enter the hight of the cylinder: "))
-radius = int(input("Enter the radius of the cylinder: "))
-pi = 3.14
-volume = pi * (radius ** 2) * hight
-print(f"The volume of the cylinder with hight {hight} and radius {radius} is: {volume}")
