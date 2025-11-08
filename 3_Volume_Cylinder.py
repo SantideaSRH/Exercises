04a998b81f79c800635674c05245cfc2ff7f5ac6
Santiago De Alba Cazares <santiago.dealbacazares@stud.srh-university.de>
2025-11-08 09:22:49 +0100

    no message

--- 3_Volume_Cylinder.py
+++ 3_Volume_Cylinder.py
@@ -1,1 +1,5 @@
-volume = pi * (radius ** 2) * hight
+hight = float(input("Enter the hight of the cylinder: "))
+radius = float(input("Enter the radius of the cylinder: "))
+pi = 3.14
+volume = str(pi * (radius ** 2) * hight)
+print(f"The volume of the cylinder with hight {hight} and radius {radius} is: {volume}")
