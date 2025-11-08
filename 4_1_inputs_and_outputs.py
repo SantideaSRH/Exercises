4d545addef31f37c298541b10abc3ec123ab3449
Santiago De Alba Cazares <santiago.dealbacazares@stud.srh-university.de>
2025-11-08 09:31:04 +0100

    input and output

---
+++ 4_1_inputs_and_outputs.py
@@ -0,0 +1,3 @@
+name = input("Enter your name: ")
+greetting = input ("Enter your prefered greetting: ")
+print (f"{greetting}, {name}!")
---
+++ 4_2_input_and_output.py
@@ -0,0 +1,8 @@
+num1 = input ("Enter the first number: ")
+num2 = input ("Enter the second number: ")
+
+sum_result = float(num1) + float(num2)
+Product = float(num1) * float(num2)
+print (f"The sum of {num1} and {num2} is {sum_result}.")
+print ("Sum", sum_result, "| product", Product, sep =" -> ")
+
