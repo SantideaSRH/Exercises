bf6eff4c986cac12162869c9a9e0e4158f59ddff
Santiago De Alba Cazares <santiago.dealbacazares@stud.srh-university.de>
2025-11-22 09:42:11 +0100

    Using for and while loops

---
+++ 5_2--FlowControl.py
@@ -0,0 +1,17 @@
+#Use a for loop with the range() function and an if statement 
+#to print all even numbers between 1 and 50, then rewrite it using a single while loop
+
+
+for num in range(1,51):
+    if num % 2 == 0:
+        print(f"{num} is even")
+
+print ("\n")
+
+#Rewriting using a while loop
+num = 1
+while num <= 50:
+    if num % 2 == 0:
+        print(f"{num} is even")
+    num += 1
+    
