0048bc484ef1a40201eef1aae61384c034fc9b70
Santiago De Alba Cazares <santiago.dealbacazares@stud.srh-university.de>
2025-11-29 10:31:58 +0100

    Fibonacci
    (cherry picked from commit 6ee29b6521a4c1106b98e28076fc41b872e0aedc)

---
+++ 7_2-Functions.py
@@ -0,0 +1,12 @@
+#Excercise 2: Recursive Fibonacci
+def fibonacci(n):
+    """computes the nth Fibonacci number recrusively"""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        # Base case is n=0 and n=1
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+print (f"Fibonacci sequence for n=4: {fibonacci(4)}")
